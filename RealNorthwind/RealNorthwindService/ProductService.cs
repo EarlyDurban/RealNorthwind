c0c99d61f6f4632686cd8b2dda0df5b133fd053a
EarlyDurban <soulfly.jah@yandex.ru>
2015-03-19 19:27:51 +0200

    Change ProductService using business logic layer

--- RealNorthwind/RealNorthwindService/ProductService.cs
+++ RealNorthwind/RealNorthwindService/ProductService.cs
@@ -4,53 +4,71 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using MyWCFServices.RealNorthwindBDO;
+using MyWCFServices.RealNorthwindLogic;
 
 namespace MyWCFServices.RealNorthwindService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class ProductService : IProductService
     {
+        ProductLogic productLogic = new ProductLogic();
         public Product GetProduct(int id) 
         {
+            ProductBDO productBDO = productLogic.GetProduct(id);
             Product product = new Product();
-            product.ProductId = id;
-            product.ProductName = "fake product from service layer";
-            product.UnitPrice = 10.0m;
-            product.QuantityPerUnit = "fake QPU";
-
+            TranslateProductBDOTToProductDTO(productBDO, product);
             return product;
         }
 
-        public bool UpdateProduct(Product product, ref string messsage) 
+        public bool UpdateProduct(Product product, ref string message) 
         {
             bool result = true;
 
             //first check to see if  it is a valid price
             if (product.UnitPrice <= 0)
             {
-                messsage = "Price cannot be <= 0";
+                message = "Price cannot be <= 0";
                 result = false;
             }
             // Product can't be empty
             else if (string.IsNullOrEmpty(product.ProductName))
             {
-                messsage = "Preoduct name can't be empty";
+                message = "Preoduct name can't be empty";
                 result = false;
             }
             // QuantityPerUnit cant't be empty
             else if (string.IsNullOrEmpty(product.QuantityPerUnit))
             {
-                messsage = "QuantityPerUnit cant't be empty";
+                message = "QuantityPerUnit cant't be empty";
                 result = false;
             }
             else
             {
-                // TODO: call business logic layer to update product
-                messsage = "Product update successfully";
-                result = true;
+                ProductBDO productBDO = new ProductBDO();
+                TranslateProductDTOToProductBDO(product, productBDO);
+                return productLogic.UpdateProduct(productBDO, ref message);
             }
 
             return result;
+        }
+
+        private void TranslateProductBDOTToProductDTO(ProductBDO productBDO, Product product) 
+        {
+            product.ProductId = productBDO.ProductID;
+            product.ProductName = productBDO.ProductName;
+            product.QuantityPerUnit = productBDO.QuantityPerUnit;
+            product.UnitPrice = productBDO.UnitPrice;
+            product.Discontinued = productBDO.Discontinued;
+        }
+
+        private void TranslateProductDTOToProductBDO(Product product, ProductBDO productBDO)
+        {
+            productBDO.ProductID = product.ProductId;
+            productBDO.ProductName = product.ProductName;
+            productBDO.QuantityPerUnit = product.QuantityPerUnit;
+            productBDO.UnitPrice = product.UnitPrice;
+            productBDO.Discontinued = product.Discontinued;
         }
     }
 }
