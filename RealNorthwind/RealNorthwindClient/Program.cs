85b1d4ae20d115f16da3129ee4a22425e2280c4c
EarlyDurban <soulfly.jah@yandex.ru>
2015-03-19 17:47:27 +0200

    project init

---
+++ RealNorthwind/RealNorthwindClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealNorthwindClient.ProductServiceRef;
+using MyWCFServices.RealNorthwindService;
+
+namespace RealNorthwindClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductServiceClient client = new ProductServiceClient();
+            Product product = client.GetProduct(23);
+
+            Console.WriteLine("product name is " + product.ProductName);
+            Console.WriteLine("product price is " + product.UnitPrice.ToString());
+            product.UnitPrice = 20.0m;
+            string message = "";
+            bool result = client.UpdateProduct(product, ref message);
+            Console.WriteLine("Update result is " + result.ToString());
+            Console.WriteLine("Update message is " + message);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RealNorthwind/RealNorthwindService/ProductService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWCFServices.RealNorthwindService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ProductService : IProductService
+    {
+        public Product GetProduct(int id) 
+        {
+            Product product = new Product();
+            product.ProductId = id;
+            product.ProductName = "fake product from service layer";
+            product.UnitPrice = 10.0m;
+            product.QuantityPerUnit = "fake QPU";
+
+            return product;
+        }
+
+        public bool UpdateProduct(Product product, ref string messsage) 
+        {
+            bool result = true;
+
+            //first check to see if  it is a valid price
+            if (product.UnitPrice <= 0)
+            {
+                messsage = "Price cannot be <= 0";
+                result = false;
+            }
+            // Product can't be empty
+            else if (string.IsNullOrEmpty(product.ProductName))
+            {
+                messsage = "Preoduct name can't be empty";
+                result = false;
+            }
+            // QuantityPerUnit cant't be empty
+            else if (string.IsNullOrEmpty(product.QuantityPerUnit))
+            {
+                messsage = "QuantityPerUnit cant't be empty";
+                result = false;
+            }
+            else
+            {
+                // TODO: call business logic layer to update product
+                messsage = "Product update successfully";
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
